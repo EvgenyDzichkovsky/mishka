ab8afb5224831dcef08c925b79de60963e526459
evgeny <makkarevi4@gmail.com>
2019-11-18 22:04:44 +0300

    redesign header

--- src/app/js/js.js
+++ src/app/js/js.js
@@ -1,14 +1,14 @@
-var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navMain = document.querySelector('.navigation');
+var navToggle = document.querySelector('.navigation__toggle');
 
-navMain.classList.remove('main-nav--nojs');
+navMain.classList.remove('navigation--nojs');
 
 navToggle.addEventListener('click', function () {
-    if (navMain.classList.contains('main-nav--closed')){
-        navMain.classList.remove('main-nav--closed');
-        navMain.classList.add('main-nav--opened');
+    if (navMain.classList.contains('navigation--closed')){
+        navMain.classList.remove('navigation--closed');
+        navMain.classList.add('navigation--opened');
     }else {
-        navMain.classList.add('main-nav--closed');
+        navMain.classList.add('navigation--closed');
         navMain.classList.remove('main-nav--opened');
     }
 });
